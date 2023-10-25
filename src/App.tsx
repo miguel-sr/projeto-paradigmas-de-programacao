4f1f0dce8c9cecf1de83976932d934863d271dc4
Jeane Cristina <jeane.o-martins@hotmail.com.br>
2023-10-25 10:42:14 -0300

    feat: adding content remaining by Jeane

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./styles/global.css";
 
 import { RouterProvider } from "react-router-dom";
 import rotas from "./routes";
+import { ModalParadigmsProvider } from "./components/ConteudoN1/conteudo-components/ModalParadigmsProvider";
+import { ModalLanguagesProvider } from "./components/ConteudoN1/conteudo-components/ModalLanguagesProvider";
 
 function App() {
-  return <RouterProvider router={rotas} />;
+  return <ModalLanguagesProvider><ModalParadigmsProvider><RouterProvider router={rotas} /></ModalParadigmsProvider></ModalLanguagesProvider>;
 }
 
 export default App;
---
+++ src/components/ConteudoN1/História.tsx
@@ -0,0 +1,9 @@
+export default function Historia() {
+    return (
+      
+        <div id="history" className="text-center relative bg-gray-100">
+            <h1 className="text-4xl h-96  inline center  ">HISTÓRIA</h1>
+        </div>
+    
+    );
+  }
