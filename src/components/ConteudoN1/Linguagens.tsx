ff47bec015d9eb4c35d620f907f2cf71221dbaee
Miguel Ramos <miguelsramos458@gmail.com>
2023-11-12 20:58:49 -0300

    12/11]

--- src/components/ConteudoN1/Linguagens.tsx
+++
@@ -1,9 +0,0 @@
-export default function Linguagens() {
-    return (
-      
-        <div id="history" className="text-center relative bg-gray-100">
-            <h1 className="text-4xl h-96  inline center  ">LINGUAGENS</h1>
-        </div>
-    
-    );
-  }
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-teal-700 py-2">
-      <p className="text-white text-center font-semibold">
-        Jeane, Marcos e Miguel
-      </p>
-    </footer>
-  );
-}
